baccfb8f99d3c0b93798f9a91a47c78b2ad7e64f
George Ogden <george@goprogram.ai>
2023-03-17 18:22:24 +0000

    install with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import numpy
 
 extensions = [
-    Extension("curling.stone", ["curling/stone.pyx"], include_dirs=[numpy.get_include()])
+    Extension("curling.stone", ["curling/stone.pyx"])
 ]
 
 setup(
-    name="curling",
     ext_modules=cythonize(extensions),
-    install_requires=[
-        "numpy",
-        "opencv-python",
-    ],
-    packages=["curling"],
-    package_dir={"curling": "curling"},
     zip_safe=False,
-    setup_requires=['Cython'],
 )
