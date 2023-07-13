fabc8df30544caaaa23045b175658caa5cd13f6e
George Ogden <george-ogden@outlook.com>
2023-07-13 14:20:00 +0200

    move from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -8,20 +8,6 @@ extensions = [
 setup(
     ext_modules=cythonize(extensions),
     zip_safe=False,
-    extras_require={
-        "test": [
-            "pytest>=6.2",
-            "pytest-timeout>=1.5",
-        ],
-        "docs": [
-            "mkdocs==1.4.3",
-            "mkdocs-material==7.3.3",
-            "mkdocstrings==0.18.0",
-            "mkdocs-git-revision-date-plugin",
-            "mkdocs-mermaid2-plugin==0.6.0",
-            "mkdocs-include-markdown-plugin==2.0.0",
-        ]
-    },
     package_dir={
         "curling": "src",
     }
